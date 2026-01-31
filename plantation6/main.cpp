bbd0a0b88e0d344c7e76abc4c61e7b48613fc7b2
Egor <egor@rangevision.com>
2026-01-31 16:16:32 +0300

    Все по новой

--- plantation6/main.cpp
+++ plantation6/main.cpp
@@ -2,15 +2,5 @@
 
 void main()
 {
-	std::cout << "Egor prepod privet" << std::endl;
-
-	std::cout << "alexey76255807 PIVET" << std::endl;
-
-	std::cout << "LeSHA poka" << std::endl;
-
-	std::cout << "125412541345134531534164136123424323424324242423432322343243212312" << std::endl;
-
-	std::cout << "test" << std::endl;
-
-	std::cout << "67 gng" << std::endl;
+	std::cout << "Vse po novoy" << std::endl;
 }
