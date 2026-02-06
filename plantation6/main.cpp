a523fd3ba37a4e6648c9f5b7e14190a7388e9582
Egor <egor@rangevision.com>
2026-02-06 19:42:49 +0300

    Добавил цикл отрисовка кадра и цикл событий

--- plantation6/main.cpp
+++ plantation6/main.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 
 void main()
 {
-
-	std::cout << "s.Novinki" << std::endl;
-
-	std::cout << "mesv privet " << std::endl;
-
+	sf::Window main_window(sf::VideoMode(500, 500), "window");
+	
+	while (main_window.isOpen()) // цикл отрисовки кадра
+	{
+		sf::Event main_window_event;
+		while (main_window.pollEvent(main_window_event)) // цикл обработки событий
+		{
+			if (main_window_event.type == sf::Event::Closed)
+			{
+				main_window.close();
+			}
+		}
+	}
 }
