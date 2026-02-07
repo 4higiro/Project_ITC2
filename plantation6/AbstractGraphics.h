3aded4ca62533bb99e903eb35878deee81d8543d
Egor <egor@rangevision.com>
2026-02-07 16:27:05 +0300

    Добавил реализацю метода event у абстракного графического объекта

--- plantation6/AbstractGraphics.h
+++ plantation6/AbstractGraphics.h
@@ -11,8 +11,8 @@ protected:
     virtual void mouseMoveEvent(unsigned x, unsigned y) {}
     virtual void mouseScrollEvent(unsigned x, unsigned y, int delta) {}
 
-    virtual void keyPressedEvent(sf::Keyboard key, bool ctrl, bool alt, bool shift) {}
-    virtual void keyReleasedEvent(sf::Keyboard key, bool ctrl, bool alt, bool shift) {}
+    virtual void keyPressedEvent(sf::Keyboard::Key key, bool ctrl, bool alt, bool shift) {}
+    virtual void keyReleasedEvent(sf::Keyboard::Key key, bool ctrl, bool alt, bool shift) {}
 
     virtual void resizeEvent(unsigned width, unsigned height) = 0;
     virtual void showEvent() = 0;
@@ -21,7 +21,30 @@ protected:
 public:
     void event(sf::Event e)
     {
-
+        switch (e.type)
+        {
+        case sf::Event::EventType::MouseButtonPressed:
+            mousePressedEvent(e.mouseButton.x, e.mouseButton.y, e.mouseButton.button);
+            break;
+        case sf::Event::EventType::MouseButtonReleased:
+            mouseReleasedEvent(e.mouseButton.x, e.mouseButton.y, e.mouseButton.button);
+            break;
+        case sf::Event::EventType::MouseMoved:
+            mouseMoveEvent(e.mouseMove.x, e.mouseMove.y);
+            break;
+        case sf::Event::EventType::MouseWheelScrolled:
+            mouseScrollEvent(e.mouseWheel.x, e.mouseWheel.y, e.mouseWheel.delta);
+            break;
+        case sf::Event::EventType::KeyPressed:
+            keyPressedEvent(e.key.code, e.key.control, e.key.alt, e.key.shift);
+            break;
+        case sf::Event::EventType::KeyReleased:
+            keyReleasedEvent(e.key.code, e.key.control, e.key.alt, e.key.shift);
+            break;
+        case sf::Event::EventType::Resized:
+            resizeEvent(e.size.width, e.size.height);
+            break;
+        }
     }
 
     virtual void draw(sf::RenderWindow* window) = 0;
