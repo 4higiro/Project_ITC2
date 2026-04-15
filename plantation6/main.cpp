dc71bad708bea5cb6d7c36215388c4f7385bbe11
Alexey <alexeybarinov762@gmail.com>
2026-04-15 23:17:56 +0300

    Добавил рисование сцены под ui

--- plantation6/main.cpp
+++ plantation6/main.cpp
@@ -1,17 +1,31 @@
-#include <iostream>
-#include <glad/glad.h>
 #include <SFML/Window.hpp>
-#include <SFML/Graphics.hpp>
-#include <SFML/Audio.hpp>
-
+#include <iostream>
 
 #include "main_menu.h"
 #include "AbstractGraphics.h"
+#include "Scene.h"
 
 void main()
 {
-	sf::RenderWindow main_window(sf::VideoMode::getDesktopMode(), "", sf::Style::Fullscreen);
+	sf::ContextSettings ogl_context;
+	ogl_context.majorVersion = 3;
+	ogl_context.minorVersion = 3;
+	ogl_context.attributeFlags = sf::ContextSettings::Default;
+	ogl_context.depthBits = 24;
+	ogl_context.stencilBits = 8;
+	ogl_context.antialiasingLevel = 4;
+
+	sf::RenderWindow main_window(sf::VideoMode::getDesktopMode(), "", sf::Style::Fullscreen, ogl_context);
 	sf::Vector2u resolution = main_window.getSize();
+
+	main_window.setActive(true);
+	Scene main_scene;
+	Camera main_camera(resolution);
+
+	Mesh* mesh_list[] = {
+		nullptr
+	};
+	constexpr int n_meshes = sizeof(mesh_list) / sizeof(Mesh*);
 
 	Button play_button(resolution);
 	Button settings_button(resolution);
@@ -20,20 +34,24 @@ void main()
 	main_menu.setPlayButton(&play_button);
 	main_menu.setSettingsButton(&settings_button);
 	main_menu.setAchievementsButton(&achievements_button);
-	
-
 
 	AbstractGraphics* draw_objects[] = {
-		
 		&main_menu,& play_button,& settings_button,& achievements_button
-		
-	
-
 	};
 	constexpr int n_draw_objects = sizeof(draw_objects) / sizeof(AbstractGraphics*);
 	
 	while (main_window.isOpen()) // цикл отрисовки кадра
 	{
+		// сцена рисуется новым OpenGL 3.3 Core
+		for (int i = 0; i < n_meshes; ++i)
+		{
+			if (!mesh_list[i]) continue;
+			main_scene.draw(main_camera, *mesh_list[i]);
+		}
+
+		// интерфейс рисуется старым OpenGL примитивами SFML
+		main_window.pushGLStates();
+
 		main_window.clear(sf::Color(255, 255, 255));
 
 		sf::Event main_window_event;
@@ -49,7 +67,7 @@ void main()
 
 			for (int i = 0; i < n_draw_objects; ++i)
 			{
-				if (draw_objects[i]->isVisible())
+				if (draw_objects[i] && draw_objects[i]->isVisible())
 				{
 					if (main_window_event.type == sf::Event::EventType::MouseButtonPressed
 					 || main_window_event.type == sf::Event::EventType::MouseButtonReleased
@@ -73,6 +91,8 @@ void main()
 			if (draw_objects[i]->isVisible())
 				draw_objects[i]->draw(&main_window);
 		}
+
+		main_window.popGLStates();
 		
 		main_window.display();
 	}
